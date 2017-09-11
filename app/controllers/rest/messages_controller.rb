cc736973fbf5eb327af921ec4c975a1d102f4f7f
yishnish <yishnish@gmail.com>
2017-09-11 00:57:55 +0000

    require password to post

--- app/controllers/rest/messages_controller.rb
+++ app/controllers/rest/messages_controller.rb
@@ -1,15 +1,17 @@
-class Rest::MessagesController < ApplicationController
-  
+class Rest::MessagesController < ApplicationController  
   before_filter :set_up_cors
+  before_filter :authenticate
   
   skip_before_filter :verify_authenticity_token, only: :create
   
   def create
-    puts message_params
-    
-    message = Message.new(message_params)
-    if(message.save)
-      render status: 200, json: {}.to_json
+    if authenticate
+      message = Message.new(message_params)
+      if(message.save)
+        render status: 200, json: {}.to_json
+      end
+    else
+      render status: 401, json: {}.to_json
     end
   end
   
@@ -17,6 +19,11 @@ private
   
   def message_params
     params.permit(:time, :user, :message)
+  end
+  
+  def authenticate
+    user = User.find_by(name: params[:username])
+    return user && user.password == params[:password]
   end
   
   def set_up_cors
--- test/controllers/rest/messages_controller_test.rb
+++ test/controllers/rest/messages_controller_test.rb
@@ -2,10 +2,15 @@ require 'test_helper'
 
 class Rest::MessagesControllerTest < ActionController::TestCase
   test 'should create a message' do
-    post :create, time: Time.zone.now, user: 'aphostle', message: 'yungler'
+    post :create, time: Time.zone.now, user: 'aphostle', message: 'yoongler', username: User.first.name, password: User.first.password
     assert_response :success
     message = Message.find_by(user: 'aphostle')
     
-    assert_equal message.message, 'yungler'
+    assert_equal message.message, 'yoongler'
+  end
+  
+  test 'it fails without correct password' do
+    post :create, time: Time.zone.now, user: 'whatever', message: 'yarrrr'
+    assert_response 401
   end
 end
