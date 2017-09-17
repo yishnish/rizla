8a3524e007c1e665ab3dd153cc007722021dbca6
yishnish <yishnish@gmail.com>
2017-09-17 16:54:34 +0000

    added host name to message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910200357) do
+ActiveRecord::Schema.define(version: 20170917165317) do
 
   create_table "messages", force: :cascade do |t|
     t.datetime "time"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170910200357) do
     t.string   "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "host"
   end
 
   add_index "messages", ["user"], name: "index_messages_on_user"
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -5,12 +5,14 @@ class MessageTest < ActiveSupport::TestCase
     time = Time.zone.now
     user = 'bobbay!'
     message = 'this game is dying!'
-    new_message = Message.new(time: time, user: user, message: message)
+    host = 'aphostle'
+    new_message = Message.new(time: time, user: user, message: message, host: host)
     
     new_message.save!
     
     assert_equal(time, new_message.time)
     assert_equal(message, new_message.message)
     assert_equal(user, new_message.user)
+    assert_equal(host, new_message.host)
   end
 end
